ce4d365daabb444091635ea696ed4bcb5762f3dc
sid7 <suyashsidzzz@gmail.com>
2019-04-01 11:21:03 +0530

    added custom scroll-spy

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,27 @@ import MyProjects from "./components/projects";
 import Contact from "./components/contact";
 
 import { links, projectsLst } from "./data";
+import scrollSpy from "./utils/scroll-spy";
+
+window.scrollSpy = scrollSpy;
 
 class App extends Component {
+  componentDidMount() {
+    scrollSpy.inject(".sec");
+  }
+  componentWillUnmount() {
+    scrollSpy.eject();
+  }
   render() {
     return (
       <>
         <Header />
         <Navbar />
         <About />
-        <MyProjects lst={projectsLst} pl={{...links.profiles, FreeCodeCamp: links.fcc}} />
+        <MyProjects
+          lst={projectsLst}
+          pl={{ ...links.profiles, FreeCodeCamp: links.fcc }}
+        />
         <Contact {...links} />
       </>
     );
---
+++ src/utils/scroll-spy.js
@@ -0,0 +1,70 @@
+const $ = q => document.querySelector(q);
+const $$ = Q => [].slice.call(document.querySelectorAll(Q));
+
+export default new class scrollSpy {
+  constructor() {
+    this.ticks = {
+      scroll: false,
+      resize: false
+    };
+    this.lst = {};
+  }
+  getActiveSec = pos => {
+    let rtn;
+    for (const i in this.lst) {
+      if (this.lst[i] <= pos) {
+        rtn = i;
+      }
+    }
+    return rtn;
+  };
+  __cls_active = () => {
+    const actEl = $(".site-nav .active");
+    if (actEl) {
+      actEl.classList.remove("active");
+    }
+  };
+  __scroll_agent = () => {
+    const pos = document.documentElement.scrollTop || document.body.scrollTop;
+    if (!this.ticks.scroll) {
+      requestAnimationFrame(() => {
+        const sec = this.getActiveSec(pos);
+        this.__cls_active();
+        const a = $(`a[href="#${sec}"]`);
+        if (a) {
+          a.classList.add("active");
+          document.body.className = `active-${sec}`;
+        } else {
+          document.body.className = "";
+        }
+        
+        this.ticks.scroll = false;
+      });
+      this.ticks.scroll = true;
+    }
+  };
+  __resize_agent = () => {
+    if (!this.ticks.resize) {
+      requestAnimationFrame(() => {
+        this.setPos();
+        this.ticks.resize = false;
+      });
+      this.ticks.resize = true;
+    }
+  };
+  setPos = () => {
+    this.secs.forEach(sec => {
+      this.lst[sec.id] = sec.offsetTop - 50;
+    });
+  };
+  inject = target => {
+    this.secs = $$(target);
+    this.setPos();
+    document.addEventListener("scroll", this.__scroll_agent);
+    document.addEventListener("resize", this.__resize_agent);
+  };
+  eject = () => {
+    document.removeEventListener("scroll", this.__scroll_agent);
+    document.removeEventListener("resize", this.__resize_agent);
+  };
+}();
