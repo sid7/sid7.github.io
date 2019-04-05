0ce4d2d4283f9822630ba241f1766872e8101af1
sid7 <suyashsidzzz@gmail.com>
2019-04-06 02:02:48 +0530

    Updates

---
+++ precache-manifest.32ba2f569335233b7810b9c086c62b99.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f192bfc1bb0d0f8d18954deb240bf28",
+    "url": "/static/media/tic_tac_toe.0f192bfc.png"
+  },
+  {
+    "revision": "016833cf293a9bc5e4d58d572a163385",
+    "url": "/static/media/restaurant_review.016833cf.png"
+  },
+  {
+    "revision": "34b11746750925d17c07467c07950211",
+    "url": "/static/media/recipe_box.34b11746.png"
+  },
+  {
+    "revision": "8ed4d905bc84d5735a2583d453b625d5",
+    "url": "/static/media/pomodoro_clock.8ed4d905.png"
+  },
+  {
+    "revision": "0d5c86619378ca083ef793f460fcfad9",
+    "url": "/static/media/game_of_life.0d5c8661.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9e24095a0053aec605c9",
+    "url": "/static/js/main.3013727e.chunk.js"
+  },
+  {
+    "revision": "18a1ee579e35619e5899",
+    "url": "/static/js/2.c0be3b51.chunk.js"
+  },
+  {
+    "revision": "9e24095a0053aec605c9",
+    "url": "/static/css/main.1f3ff0cd.chunk.css"
+  },
+  {
+    "revision": "b6167ed8abf5585bc57412796fcaec73",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import "./utils/fa-lib";
-import App from "./App";
-import "./styles/index.scss";
-
-render(<App />, document.getElementById("root"));
