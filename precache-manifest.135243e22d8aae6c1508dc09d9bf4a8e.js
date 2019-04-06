2097a6944b81dba134c9fee51eb312375f999958
sid7 <suyashsidzzz@gmail.com>
2019-04-06 21:59:57 +0530

    Updates

---
+++ precache-manifest.135243e22d8aae6c1508dc09d9bf4a8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f192bfc1bb0d0f8d18954deb240bf28",
+    "url": "/static/media/tic_tac_toe.0f192bfc.png"
+  },
+  {
+    "revision": "016833cf293a9bc5e4d58d572a163385",
+    "url": "/static/media/restaurant_review.016833cf.png"
+  },
+  {
+    "revision": "34b11746750925d17c07467c07950211",
+    "url": "/static/media/recipe_box.34b11746.png"
+  },
+  {
+    "revision": "8ed4d905bc84d5735a2583d453b625d5",
+    "url": "/static/media/pomodoro_clock.8ed4d905.png"
+  },
+  {
+    "revision": "0d5c86619378ca083ef793f460fcfad9",
+    "url": "/static/media/game_of_life.0d5c8661.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6e4d9b317c46b07454b7",
+    "url": "/static/js/main.b49b70b5.chunk.js"
+  },
+  {
+    "revision": "18a1ee579e35619e5899",
+    "url": "/static/js/2.c0be3b51.chunk.js"
+  },
+  {
+    "revision": "6e4d9b317c46b07454b7",
+    "url": "/static/css/main.6efa2f5e.chunk.css"
+  },
+  {
+    "revision": "77aeee940de542dca841164c727d0782",
+    "url": "/index.html"
+  }
+];
